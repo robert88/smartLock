dc8e088d0900ddbf43f442125efb3ed8ae876e90
robert <841354162@qq.com>
2018-05-08 22:24:27 +0800

    r

--- bin/rap.util.core.config.js
+++ bin/rap.util.core.config.js
@@ -41,8 +41,8 @@ if(!process.env.DEBUG){
 }
 rap.staticPathArr = [];
 // rap.staticPathArr.push((rap.rootPath + "/lock-pc" ));
-// rap.staticPathArr.push((rap.rootPath + "/lock-wap" ));
-rap.staticPathArr.push((rap.rootPath + "/nodeTool/build/lock-wap" ));
+rap.staticPathArr.push((rap.rootPath + "/lock-wap" ));
+// rap.staticPathArr.push((rap.rootPath + "/nodeTool/build/lock-wap" ));
 // rap.staticPathArr.push((rap.rootPath + "/shanxi-wap" ));
 // rap.staticPathArr.push((rap.rootPath + "/shanxi" ));
 // rap.staticPathArr.push((rap.rootPath + "/" + rap.staticPath ));
